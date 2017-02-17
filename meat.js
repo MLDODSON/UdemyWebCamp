d95738fe1e121f462278615741fbe61814d15fa7
MLDODSON <mldodson@fullsail.edu>
2017-02-16 20:10:12 -0600

    js practice

---
+++ meat.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function meatwad(one, two, three) {
+		document.write(one + " really loves " + two + " and " + three + "." + "<br>");		
+	}
+
+	meatwad("Faith", "steak", "shrimp");
+	meatwad("Hulk", "Betty Ross", "pinto beans");
+	meatwad("Peter", "Mary Jane", "web slinging");
---
+++ myScript.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+function myFunction() {
+	document.getElementById("learn").innerHTML="The paragraph keeps changing.";
+}
