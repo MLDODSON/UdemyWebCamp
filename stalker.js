81d42d7b4ac70194a6eeec6a0e5a366861fd9042
MLDODSON <mldodson@fullsail.edu>
2017-03-24 00:35:43 -0500

    js stalker

---
+++ stalker.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+var gender = prompt("What gender are you?");
+
+console.log("Your full name is " + firstName + " " + lastName + ".");
+console.log("You are " + age + " years old.");
+console.log("You are a " + gender + ".");
+
