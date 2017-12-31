099dd6da778abfde81f0ee2c21b9e7d3894b8870
MLDODSON <mldodson@fullsail.edu>
2017-12-30 19:54:46 -0600

    jQuery practice exercise

---
+++ jqpractice.js
@@ -0,0 +1,11 @@
+//Select all divs and give them purple backgrounds
+$("div").css("background", "purple");
+
+//Select divs with the class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");	
+	
+//Select the div with the id "third" and give it an orange border
+$("#third").css("border", "3px solid orange");
+
+//Select the first div only and make it pink
+$("div:first-of-type").css("color", "pink");
