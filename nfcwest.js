a1aaced024c3116a2740a60b616490e1474a93b0
MLDODSON <mldodson@fullsail.edu>
2017-12-31 00:46:10 -0600

    NFC West project w/jQuery

---
+++ nfcwest.js
@@ -0,0 +1,42 @@
+$("h1").css("color", "rgb(27, 31, 132)");
+$("h1").css("font-family", "Roboto Slab");
+$("h1").css("font-size", "70px");
+$("h1").css("text-shadow", "2px 2px 10px white");
+$("h1").css("margin", "100px 0px 50px 0px");
+
+$(".container").css("text-align", "center");
+
+$(".row").css("margin-top", "50px");
+
+$("div").css("margin-bottom", "25px");
+$("div").css("padding", "5px 0px 5px 0px");
+$("div").css("text-transform", "uppercase");
+$("div").css("font-size", "30px");
+$("div").css("font-family", "Roboto");
+
+
+
+$("#blue").css("background", "yellow");
+$("#blue").css("color", "rgb(33, 22, 117)");
+$("#blue").css("border", "3px solid rgb(33, 22, 117)");
+$("#blue").css("box-shadow", "inset 0.2em 0.2em 0.2em 0 rgba(255,255,255,0.5), inset -0.2em -0.2em 0.2em 0 rgba(0,0,0,0.5");
+
+$(".green").css("background", "rgb(164, 244, 66)");
+$(".green").css("color", "rgb(27, 31, 132)");
+$(".green").css("border", "3px solid rgb(27, 31, 132)");
+$(".green").css("box-shadow", "inset 0.2em 0.2em 0.2em 0 rgba(255,255,255,0.5), inset -0.2em -0.2em 0.2em 0 rgba(0,0,0,0.5");
+
+$("#red").css("background", "rgb(181, 5, 31)");
+$("#red").css("color", "white");
+$("#red").css("border", "3px solid black");
+$("#red").css("box-shadow", "inset 0.2em 0.2em 0.2em 0 rgba(255,255,255,0.5), inset -0.2em -0.2em 0.2em 0 rgba(0,0,0,0.5");
+
+$(".gold").css("background", "rgb(232, 2, 2)");
+$(".gold").css("color", "rgb(249, 213, 4)");
+$(".gold").css("border", "3px solid rgb(249, 213, 4)");
+$(".gold").css("box-shadow", "inset 0.2em 0.2em 0.2em 0 rgba(255,255,255,0.5), inset -0.2em -0.2em 0.2em 0 rgba(0,0,0,0.5");
+	
+
+	
+
+
