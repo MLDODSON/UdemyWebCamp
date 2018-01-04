e31e59928c1d69f46c0270c5df16e405a6b5e35f
MLDODSON <mldodson@fullsail.edu>
2018-01-04 14:38:57 -0600

    to do project, part 3

--- mytodo.js
+++ mytodo.js
@@ -1,10 +1,10 @@
 // Check off specific Todos by clicking.
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 	  $(this).remove();
 	});
@@ -13,6 +13,10 @@ $("span").click(function(event){
 
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
-	  console.log("You hit enter!");
+	  //grabbing new todo text from input
+	  var todoText = $(this).val();
+		$(this).val("");
+	  //create a new li and add to ul
+	  $("ul").append("<li><span>X</span> " + todoText + "</li>");
 	}
 });
