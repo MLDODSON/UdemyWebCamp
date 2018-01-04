070281fe9163a3f0805e5c59ef23f063b759c8e2
MLDODSON <mldodson@fullsail.edu>
2018-01-04 13:04:04 -0600

    to do project, part 2

--- mytodo.js
+++ mytodo.js
@@ -1,1 +1,18 @@
-// JavaScript Document
+// Check off specific Todos by clicking.
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+	  $(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+	  console.log("You hit enter!");
+	}
+});
